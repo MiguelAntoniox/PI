4812184acfd619857d3d603a50d261803dfd089e
Lucas Matheus <36493471+peresmorais@users.noreply.github.com>
2025-11-25 21:25:36 -0300

    Refactor Reits class initialization

--- STOPLO/reits.py
+++ STOPLO/reits.py
@@ -7,16 +7,16 @@ import tkinter as tk
 
 class Reits:
     """atributos da classe moedas"""
-   
+    
     def __init__(self):
         # self.tipoacaoa = tipoacao
         self.janelaacoes = tk.Toplevel()
         self.janelaacoes.title("Ações STOP.LO")
         self.janelaacoes.geometry("400x300")
         self.janelaacoes.configure(bg = "white")
-       
-       
-       
-       
+        
+        
+        
+        
     def iniciar(self):
         self.janelaacoes.mainloop()
